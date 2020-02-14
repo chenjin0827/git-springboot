c5e94ef50d0b0459b8a3afa19a07ee52710ac106
1219650953@qq.com <cj13032716642>
2020-02-14 12:49:56 +0800

    20200214

--- springbootvip/src/main/java/cn/enjoy/App.java
+++ springbootvip/src/main/java/cn/enjoy/App.java
@@ -37,3 +37,21 @@ public class App {
     }
 
 }
+/**
+ * springboot 打成war包启动类写法
+ */
+
+/*@SpringBootApplication
+@MapperScan("cn.enjoy.dao")
+public class App  extends SpringBootServletInitializer  {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+}*/
